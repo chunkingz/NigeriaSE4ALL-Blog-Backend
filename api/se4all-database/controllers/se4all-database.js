e918cbf3fca4a55b2d566a7f4663093d84d15b35
AlexanderWeiss-Motz <aweiss-motz@integration.org>
2022-05-31 14:48:37 +0200

    se4all database api reworked

--- api/se4all-database/controllers/se4all-database.js
+++ api/se4all-database/controllers/se4all-database.js
@@ -26,22 +26,33 @@ module.exports = {
         (async () => {
             var client = await pool.connect()
             try {
-              var gauge_data = await client.query('SELECT  * FROM  se4all.website_dashboard_gauge_v')
-              var plot_data = await client.query('SELECT  * FROM  se4all.website_dashboard_plots_mw ')
+              var gauge_data = await client.query('SELECT  * FROM  se4all.website_dashboard_gauge_v');
+              var plot_data = await client.query('SELECT  * FROM  se4all.website_dashboard_plots_mw');
 
+              var cap_realized = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.cap_realized}));
+              var cap_projected = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.cap_projected}));
+              var per_regen_realized = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.per_regen_realized}));
+              var per_regen_projected = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.per_regen_projected}));
 
-              var cap = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.cap}));
-              var per_regen = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.per_regen}));
-              var cap_per_cat_pv = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.capacity_pv_mw}));
-              var cap_per_cat_hydro = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.capacity_hydro_mw}));
-              var cap_per_cat_wind = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.capacity_wind_mw}));
+              var cap_per_cat_pv_realized = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.capacity_pv_mw_realized}));
+              var cap_per_cat_hydro_realized = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.capacity_hydro_mw_realized}));
+              var cap_per_cat_wind_realized = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.capacity_wind_mw_realized}));
+              var cap_per_cat_pv_projected = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.capacity_pv_mw_projected}));
+              var cap_per_cat_hydro_projected = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.capacity_hydro_mw_projected}));
+              var cap_per_cat_wind_projected = plot_data.rows.map(x => ({key: x.start_year_of_operation, value: x.capacity_wind_mw_projected}));
 
               series = {};
-              series['Capacity'] = cap;
-              series['Renewable'] = per_regen;
-              series['PV'] = cap_per_cat_pv;
-              series['Hydro'] = cap_per_cat_hydro;
-              series['Wind'] = cap_per_cat_wind;
+              series['Capacity_Realized'] = cap_realized;
+              series['Capacity_Projected'] = cap_projected;
+              series['Renewable_Realized'] = per_regen_realized;
+              series['Renewable_Projected'] = per_regen_projected;
+
+              series['PV_Realized'] = cap_per_cat_pv_realized;
+              series['Hydro_Realized'] = cap_per_cat_hydro_realized;
+              series['Wind_Realized'] = cap_per_cat_wind_realized;
+              series['PV_Projected'] = cap_per_cat_pv_projected;
+              series['Hydro_Projected'] = cap_per_cat_hydro_projected;
+              series['Wind_Projected'] = cap_per_cat_wind_projected;
 
 
               response['current_generation'] = {name: 'current_generation', value: gauge_data.rows[0].current_generation};
